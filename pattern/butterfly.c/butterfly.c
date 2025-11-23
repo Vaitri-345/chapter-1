92f887198d0c9e83a85d6b0beecb25791f9899e5
Vaibhav Tripathi <tripathivaibhav678@gmail.com>
2025-11-23 11:25:50 +0530

    23-11-25

--- pattern/butterfly.c/butterfly.c
+++ pattern/butterfly.c/butterfly.c
@@ -3,17 +3,21 @@ int main(){
     int n,i,j;
     scanf("%d",&n);
     for(i=1;i<=n;i++){
-        for(j=1;j<i;j++){
+        for(j=1;j<=i;j++){
             printf("*");
         }for(j=1;j<=2*(n-i);j++){
-            printf(" ");
+            printf("-");
         }for(j=1;j<=i;j++){
             printf("*");
         }printf("\n");
     }
-    for(i=n;i>=0;i--){
-        for(j=1;j<i;j++){
+    for(i=n;i>=1;i--){
+        for(j=1;j<=i;j++){
             printf("*");              
-        }for
+        }for(j=1;j<=2*(n-i);j++){
+            printf("-");
+        }for(j=1;j<=i;j++){
+            printf("*");
+        }printf("\n");
     }
 }
